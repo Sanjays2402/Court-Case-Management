135cc80727986f05f63fb4f0e0a86b8283d4df93
Sanjay <sanjays2402@gmail.com>
2020-01-29 18:47:33 +0530

    Create index.php

---
+++ PHP/index.php
@@ -0,0 +1,275 @@
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <style media="screen">
+    #navv{
+      background: red;
+    }
+    .site-footer
+    {
+      background-color:#26272b;
+      padding:45px 0 20px;
+      font-size:15px;
+      line-height:24px;
+      color:#737373;
+    }
+    .site-footer hr
+    {
+      border-top-color:#bbb;
+      opacity:0.5
+    }
+    .site-footer hr.small
+    {
+      margin:20px 0
+    }
+    .site-footer h6
+    {
+      color:#fff;
+      font-size:16px;
+      text-transform:uppercase;
+      margin-top:5px;
+      letter-spacing:2px
+    }
+    .site-footer a
+    {
+      color:#737373;
+    }
+    .site-footer a:hover
+    {
+      color:#3366cc;
+      text-decoration:none;
+    }
+    .footer-links
+    {
+      padding-left:0;
+      list-style:none
+    }
+    .footer-links li
+    {
+      display:block
+    }
+    .footer-links a
+    {
+      color:#737373
+    }
+    .footer-links a:active,.footer-links a:focus,.footer-links a:hover
+    {
+      color:#3366cc;
+      text-decoration:none;
+    }
+    .footer-links.inline li
+    {
+      display:inline-block
+    }
+    .site-footer .social-icons
+    {
+      text-align:right
+    }
+    .site-footer .social-icons a
+    {
+      width:40px;
+      height:40px;
+      line-height:40px;
+      margin-left:6px;
+      margin-right:0;
+      border-radius:100%;
+      background-color:#33353d
+    }
+    .copyright-text
+    {
+      margin:0
+    }
+    @media (max-width:991px)
+    {
+      .site-footer [class^=col-]
+      {
+        margin-bottom:30px
+      }
+    }
+    @media (max-width:767px)
+    {
+      .site-footer
+      {
+        padding-bottom:0
+      }
+      .site-footer .copyright-text,.site-footer .social-icons
+      {
+        text-align:center
+      }
+    }
+    .social-icons
+    {
+      padding-left:0;
+      margin-bottom:0;
+      list-style:none
+    }
+    .social-icons li
+    {
+      display:inline-block;
+      margin-bottom:4px
+    }
+    .social-icons li.title
+    {
+      margin-right:15px;
+      text-transform:uppercase;
+      color:#96a2b2;
+      font-weight:700;
+      font-size:13px
+    }
+    .social-icons a{
+      background-color:#eceeef;
+      color:#818a91;
+      font-size:16px;
+      display:inline-block;
+      line-height:44px;
+      width:44px;
+      height:44px;
+      text-align:center;
+      margin-right:8px;
+      border-radius:100%;
+      -webkit-transition:all .2s linear;
+      -o-transition:all .2s linear;
+      transition:all .2s linear
+    }
+    .social-icons a:active,.social-icons a:focus,.social-icons a:hover
+    {
+      color:#fff;
+      background-color:#29aafe
+    }
+    .social-icons.size-sm a
+    {
+      line-height:34px;
+      height:34px;
+      width:34px;
+      font-size:14px
+    }
+    .social-icons a.facebook:hover
+    {
+      background-color:#3b5998
+    }
+    .social-icons a.twitter:hover
+    {
+      background-color:#00aced
+    }
+    .social-icons a.linkedin:hover
+    {
+      background-color:#007bb6
+    }
+    .social-icons a.dribbble:hover
+    {
+      background-color:#ea4c89
+    }
+    @media (max-width:767px)
+    {
+      .social-icons li.title
+      {
+        display:block;
+        margin-right:0;
+        font-weight:600
+      }
+    }
+    </style>
+    <meta charset="utf-8">
+    <title>Home Page</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>  </head>
+  <body>
+		<nav class="navbar navbar-expand-sm bg-light" id="navv">
+      <div class="container">
+      <div class="navbar-header">
+        <a href="#" class='navbar-brand'><img src="logo.png" style="width:38px;height:38px;">    Home</a>
+      </div>
+      <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="login.php"><img src="licon.jpg" style="width:32px;height:32px;">Login</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="signup.php"><img src="sup.png" style="width:32px;height:32px;">Signup</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="contact.php"><img src="cicon.jpg" style="width:32px;height:32px;">Contact Us</a>
+    </li>
+    <li>
+
+
+    </li>
+  </ul>
+      </div>
+    </nav>
+    <br><br>
+    <div class="container">
+
+    <h2>CASE MANAGEMENT SYSTEM</h2>
+    <h3>INTRODUCTION OF CASE INFORMATION MANAGEMENT SYSTEM:</h3>
+    <p>Case  Information Management System is cloud based software. This software is meant to be used by Lawyers or Advocates. This is very powerful software. Any client(Lawyer) who is using this software does not need to worry about maintaining any paperwork. This software would maintain everything which an advocate needs to do.
+
+This software will store the data about advocate’s client, opponents, Case no., case details, Previous hearing date, next hearing date, court name, employee details, case related document. This software is secure to store data.</p>
+    <p>Every sensitive information is encrypted using cryptography. This software makes easy to search about the case or case details using the search option. You can search by case no., next hearing date, client, or employee (junior advocate).
+
+The software would also manage the day to day meetings, appointments. This system will notify the user for appointments, meeting. The client can search about advocate, view their profiles, and book their slot if available. If an advocate is ready to take the case.
+
+An advocate can verify it. Then client pays the advance fee using online payment portal. Finally, Software would help the advocates/lawyers in maintaining their offices.</p>
+    <!--<p>Under Construction</p>-->
+
+  </div>
+    <br><br><br><br><br><br><br><br>
+      <!-- Site footer -->
+      <!-- Site footer -->
+    <footer class="site-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12 col-md-6">
+            <h6>About</h6>
+            <p class="text-justify"><i>Court Case</i>
+Repository of departmental court cases Design and prepare a court case management software that has facility to record information like adding a case, adding lawyers (have facility select from existing list of lawyers), add invoice for for each hearing and for different heads under which lawyers charge the clients. In short the system should provide end to end management of court case from client perspective and should be easy to use.</p>
+          </div>
+
+          <!--<div class="col-xs-6 col-md-3">-->
+          <!--  <h6>Categories</h6>-->
+          <!--  <ul class="footer-links">-->
+          <!--    <li><a href="http://scanfcode.com/category/c-language/">C</a></li>-->
+          <!--    <li><a href="http://scanfcode.com/category/front-end-development/">UI Design</a></li>-->
+          <!--    <li><a href="http://scanfcode.com/category/back-end-development/">PHP</a></li>-->
+          <!--    <li><a href="http://scanfcode.com/category/java-programming-language/">Java</a></li>-->
+          <!--    <li><a href="http://scanfcode.com/category/android/">Android</a></li>-->
+          <!--    <li><a href="http://scanfcode.com/category/templates/">Templates</a></li>-->
+          <!--  </ul>-->
+          <!--</div>-->
+
+          <!--<div class="col-xs-6 col-md-3">-->
+          <!--  <h6>Quick Links</h6>-->
+          <!--  <ul class="footer-links">-->
+          <!--    <li><a href="http://scanfcode.com/about/">About Us</a></li>-->
+          <!--    <li><a href="http://scanfcode.com/contact/">Contact Us</a></li>-->
+          <!--    <li><a href="http://scanfcode.com/contribute-at-scanfcode/">Contribute</a></li>-->
+          <!--    <li><a href="http://scanfcode.com/privacy-policy/">Privacy Policy</a></li>-->
+          <!--    <li><a href="http://scanfcode.com/sitemap/">Sitemap</a></li>-->
+          <!--  </ul>-->
+          <!--</div>-->
+        </div>
+        <hr>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-sm-6 col-xs-12">
+            <p class="copyright-text">Copyright &copy; 2020 All Rights Reserved by
+         <a href="#">Supreme Court</a>.
+            </p>
+          </div>
+
+          <div class="col-md-4 col-sm-6 col-xs-12">
+            <ul class="social-icons">
+              <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
+              <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+              <li><a class="dribbble" href="#"><i class="fa fa-dribbble"></i></a></li>
+              <li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+</footer>
+  </body>
+</html>
