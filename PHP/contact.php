d00225d6d463b9c5895629427a67feb308756c22
Sanjay <sanjays2402@gmail.com>
2020-01-30 18:46:02 +0530

    Create contact.php

---
+++ PHP/contact.php
@@ -0,0 +1,272 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <style media="screen">
+      #head{
+          /* color: red; */
+        font-family: 'Lato', sans-serif;
+      }
+     .site-footer
+    {
+      background-color:#26272b;
+      padding:45px 0 20px;
+      font-size:15px;
+      line-height:24px;
+      color:#737373;
+    }
+    .site-footer hr
+    {
+      border-top-color:#bbb;
+      opacity:0.5
+    }
+    .site-footer hr.small
+    {
+      margin:20px 0
+    }
+    .site-footer h6
+    {
+      color:#fff;
+      font-size:16px;
+      text-transform:uppercase;
+      margin-top:5px;
+      letter-spacing:2px
+    }
+    .site-footer a
+    {
+      color:#737373;
+    }
+    .site-footer a:hover
+    {
+      color:#3366cc;
+      text-decoration:none;
+    }
+    .footer-links
+    {
+      padding-left:0;
+      list-style:none
+    }
+    .footer-links li
+    {
+      display:block
+    }
+    .footer-links a
+    {
+      color:#737373
+    }
+    .footer-links a:active,.footer-links a:focus,.footer-links a:hover
+    {
+      color:#3366cc;
+      text-decoration:none;
+    }
+    .footer-links.inline li
+    {
+      display:inline-block
+    }
+    .site-footer .social-icons
+    {
+      text-align:right
+    }
+    .site-footer .social-icons a
+    {
+      width:40px;
+      height:40px;
+      line-height:40px;
+      margin-left:6px;
+      margin-right:0;
+      border-radius:100%;
+      background-color:#33353d
+    }
+    .copyright-text
+    {
+      margin:0
+    }
+    @media (max-width:991px)
+    {
+      .site-footer [class^=col-]
+      {
+        margin-bottom:30px
+      }
+    }
+    @media (max-width:767px)
+    {
+      .site-footer
+      {
+        padding-bottom:0
+      }
+      .site-footer .copyright-text,.site-footer .social-icons
+      {
+        text-align:center
+      }
+    }
+    .social-icons
+    {
+      padding-left:0;
+      margin-bottom:0;
+      list-style:none
+    }
+    .social-icons li
+    {
+      display:inline-block;
+      margin-bottom:4px
+    }
+    .social-icons li.title
+    {
+      margin-right:15px;
+      text-transform:uppercase;
+      color:#96a2b2;
+      font-weight:700;
+      font-size:13px
+    }
+    .social-icons a{
+      background-color:#eceeef;
+      color:#818a91;
+      font-size:16px;
+      display:inline-block;
+      line-height:44px;
+      width:44px;
+      height:44px;
+      text-align:center;
+      margin-right:8px;
+      border-radius:100%;
+      -webkit-transition:all .2s linear;
+      -o-transition:all .2s linear;
+      transition:all .2s linear
+    }
+    .social-icons a:active,.social-icons a:focus,.social-icons a:hover
+    {
+      color:#fff;
+      background-color:#29aafe
+    }
+    .social-icons.size-sm a
+    {
+      line-height:34px;
+      height:34px;
+      width:34px;
+      font-size:14px
+    }
+    .social-icons a.facebook:hover
+    {
+      background-color:#3b5998
+    }
+    .social-icons a.twitter:hover
+    {
+      background-color:#00aced
+    }
+    .social-icons a.linkedin:hover
+    {
+      background-color:#007bb6
+    }
+    .social-icons a.dribbble:hover
+    {
+      background-color:#ea4c89
+    }
+    @media (max-width:767px)
+    {
+      .social-icons li.title
+      {
+        display:block;
+        margin-right:0;
+        font-weight:600
+      }
+    }
+    </style>
+    <meta charset="utf-8">
+    <title></title>
+    <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+
+    <nav class="navbar navbar-expand-sm bg-light" id="navv">
+      <div class="container">
+      <div class="navbar-header">
+        <a href="clientlogin.php" class='navbar-brand'><img src="logo.png" style="width:38px;height:38px;">    Home</a>
+      </div>
+      <ul class="navbar-nav">
+    <!-- <li class="nav-item">
+      <a class="nav-link" href="login2.html">Login</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="signup.html">Signup</a>
+    </li> -->
+
+    <li>
+      <!-- <a class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a> -->
+    </li>
+  </ul>
+      </div>
+    </nav>
+    <div class="container" id="head">
+
+    <h1>Contact Us.. </h1>
+    <ul style="list-style-type:circle;">
+        <li>Sanjay - 8765432190</li>
+        <li>Sathish - 9889123412</li>
+        <li>Sathya - 9856432890</li>
+        <li>Srutikaa - 8939025911</li>
+        <li>Sujitha Sree - 879654567</li>
+        <li>Thyagaraj - 6879543255</li>
+    </ul>
+    <br><br>
+    <p>The most important part was Ius Civile (latin for "civil law"). This consisted of Mos Maiorum (latin for "way of the ancestors") and Leges (latin for "laws"). Mos Maiorum was the rules of conduct based on social norms created over the years by predecessors. In 451-449 BC, the Mos Maiorum was written down in the Twelve Tables.[1][2][3] Leges were rules set by the leaders, first the kings, later the popular assembly during the Republic. In these early years, the legal process consisted of two phases. The first phase, In Iure, was the judicial process. One would go to the head of the judicial system (at first the priests as law was part of religion) who would look at the applicable rules to the case. Parties in the case could be assisted by jurists.[4] Then the second phase would start, the Apud Iudicem. The case would be put before the judges, which were normal Roman citizens in an uneven number. No experience was required as the applicable rules were already selected. They would merely have to judge the case.[5]</p>
+    </div>
+    <br><br><br><br><br>
+      <!-- Site footer -->
+    <footer class="site-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12 col-md-6">
+            <h6>About</h6>
+            <p class="text-justify"><i>Court Case </i>
+Repository of departmental court cases Design and prepare a court case management software that has facility to record information like adding a case, adding lawyers (have facility select from existing list of lawyers), add invoice for for each hearing and for different heads under which lawyers charge the clients. In short the system should provide end to end management of court case from client perspective and should be easy to use.</p>
+          </div>
+
+      <!--    <div class="col-xs-6 col-md-3">-->
+      <!--      <h6>Categories</h6>-->
+      <!--      <ul class="footer-links">-->
+      <!--        <li><a href="http://scanfcode.com/category/c-language/">C</a></li>-->
+      <!--        <li><a href="http://scanfcode.com/category/front-end-development/">UI Design</a></li>-->
+      <!--        <li><a href="http://scanfcode.com/category/back-end-development/">PHP</a></li>-->
+      <!--        <li><a href="http://scanfcode.com/category/java-programming-language/">Java</a></li>-->
+      <!--        <li><a href="http://scanfcode.com/category/android/">Android</a></li>-->
+      <!--        <li><a href="http://scanfcode.com/category/templates/">Templates</a></li>-->
+      <!--      </ul>-->
+      <!--    </div>-->
+
+      <!--    <div class="col-xs-6 col-md-3">-->
+      <!--      <h6>Quick Links</h6>-->
+      <!--      <ul class="footer-links">-->
+      <!--        <li><a href="http://scanfcode.com/about/">About Us</a></li>-->
+      <!--        <li><a href="http://scanfcode.com/contact/">Contact Us</a></li>-->
+      <!--        <li><a href="http://scanfcode.com/contribute-at-scanfcode/">Contribute</a></li>-->
+      <!--        <li><a href="http://scanfcode.com/privacy-policy/">Privacy Policy</a></li>-->
+      <!--        <li><a href="http://scanfcode.com/sitemap/">Sitemap</a></li>-->
+      <!--      </ul>-->
+      <!--    </div>-->
+      <!--  </div>-->
+      <!--  <hr>-->
+      <!--</div>-->
+      <!--<div class="container">-->
+      <!--  <div class="row">-->
+      <!--    <div class="col-md-8 col-sm-6 col-xs-12">-->
+      <!--      <p class="copyright-text">Copyright &copy; 2017 All Rights Reserved by -->
+      <!--   <a href="#">Scanfcode</a>.-->
+      <!--      </p>-->
+      <!--    </div>-->
+
+      <!--    <div class="col-md-4 col-sm-6 col-xs-12">-->
+      <!--      <ul class="social-icons">-->
+      <!--        <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>-->
+      <!--        <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>-->
+      <!--        <li><a class="dribbble" href="#"><i class="fa fa-dribbble"></i></a></li>-->
+      <!--        <li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>   -->
+      <!--      </ul>-->
+      <!--    </div>-->
+        </div>
+      </div>
+</footer>
+  </body>
+</html>
